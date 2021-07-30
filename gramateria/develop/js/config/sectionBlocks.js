204d011920e37b07e66b88a592a27e35edbf117c
ronaldaug <saisenfa@gmail.com>
2021-07-30 22:52:25 +0630

    fix responsive for team section

--- gramateria/develop/js/config/sectionBlocks.js
+++ gramateria/develop/js/config/sectionBlocks.js
@@ -453,90 +453,109 @@ export default [
     attributes:{
       class:'fa fa-th'
     },
-    content:`<div class="container my-5">
-    <div class="row">
-      <div class="col-12">
-        <h1 class="text-center">OUR AMAZING TEAM</h1>
-      </div>
+    content:`<section id="team">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">OUR AMAZING TEAM</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+
+                <div class="box p-3 shadow bg-white rounded my-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="https://res.cloudinary.com/dpnea22ek/image/upload/v1627660134/user1.png" alt=""
+                                width="100%">
+                        </div>
+                        <div class="col-md-8">
+                            <h4>Peter J. Lamy</h4>
+                            <p class="my-2"><small>Software Developer</small></p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing tristique hendrerit laoreet.</p>
+                            <div class="social-icons">
+                                <a href="#" class="me-2"><span class="fa fa-facebook"></span></a><a href="#"
+                                    class="me-2"><span class="fa fa-instagram"></span></a><a href="#" class="me-2"><span
+                                        class="fa fa-twitter"></span></a><a href="#" class="me-2"><span
+                                        class="fa fa-linkedin"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-6">
+
+                <div class="box p-3 shadow bg-white rounded my-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="https://res.cloudinary.com/dpnea22ek/image/upload/v1627660145/user4.png" alt=""
+                                width="100%">
+                        </div>
+                        <div class="col-md-8">
+                            <h4>June D. Smith</h4>
+                            <p class="my-2"><small>Product Manager</small></p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing tristique hendrerit laoreet.</p>
+                            <div class="social-icons">
+                                <a href="#" class="me-2"><span class="fa fa-facebook"></span></a><a href="#"
+                                    class="me-2"><span class="fa fa-instagram"></span></a><a href="#" class="me-2"><span
+                                        class="fa fa-twitter"></span></a><a href="#" class="me-2"><span
+                                        class="fa fa-linkedin"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-6">
+
+                <div class="box p-3 shadow bg-white rounded my-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="https://res.cloudinary.com/dpnea22ek/image/upload/v1627660134/user3.png" alt=""
+                                width="100%">
+                        </div>
+                        <div class="col-md-8">
+                            <h4>Dianna C. Avila</h4>
+                            <p class="my-2"><small>Graphic Designer</small></p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing tristique hendrerit laoreet.</p>
+                            <div class="social-icons">
+                                <a href="#" class="me-2"><span class="fa fa-facebook"></span></a><a href="#"
+                                    class="me-2"><span class="fa fa-instagram"></span></a><a href="#" class="me-2"><span
+                                        class="fa fa-twitter"></span></a><a href="#" class="me-2"><span
+                                        class="fa fa-linkedin"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-6">
+
+                <div class="box p-3 shadow bg-white rounded my-3">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="https://res.cloudinary.com/dpnea22ek/image/upload/v1627660147/user2.png" alt=""
+                                width="100%">
+                        </div>
+                        <div class="col-md-8">
+                            <h4>Robert C. Young</h4>
+                            <p class="my-2"><small>Web Designer</small></p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing tristique hendrerit laoreet.</p>
+                            <div class="social-icons">
+                                <a href="#" class="me-2"><span class="fa fa-facebook"></span></a><a href="#"
+                                    class="me-2"><span class="fa fa-instagram"></span></a><a href="#" class="me-2"><span
+                                        class="fa fa-twitter"></span></a><a href="#" class="me-2"><span
+                                        class="fa fa-linkedin"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
-    <div class="row">
-      <div class="col-6">
-  
-        <div class="box p-3 shadow bg-white rounded my-3">
-          <div class="row">
-            <div class="col-4">
-              <img src="https://res.cloudinary.com/dpnea22ek/image/upload/v1627660134/user1.png" alt="" width="100%">
-            </div>
-            <div class="col-8">
-              <h4>Peter J. Lamy</h4>
-              <p class="my-2"><small>Software Developer</small></p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing tristique hendrerit laoreet.</p>
-              <div class="social-icons">
-                <a href="#" class="me-2"><span class="fa fa-facebook"></span></a><a href="#" class="me-2"><span class="fa fa-instagram"></span></a><a href="#" class="me-2"><span class="fa fa-twitter"></span></a><a href="#" class="me-2"><span class="fa fa-linkedin"></span></a>
-              </div>
-            </div>
-          </div>
-        </div>
-  
-      </div>
-       <div class="col-6">
-  
-        <div class="box p-3 shadow bg-white rounded my-3">
-          <div class="row">
-            <div class="col-4">
-              <img src="https://res.cloudinary.com/dpnea22ek/image/upload/v1627660145/user4.png" alt="" width="100%">
-            </div>
-            <div class="col-8">
-              <h4>June D. Smith</h4>
-              <p class="my-2"><small>Product Manager</small></p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing tristique hendrerit laoreet.</p>
-              <div class="social-icons">
-                <a href="#" class="me-2"><span class="fa fa-facebook"></span></a><a href="#" class="me-2"><span class="fa fa-instagram"></span></a><a href="#" class="me-2"><span class="fa fa-twitter"></span></a><a href="#" class="me-2"><span class="fa fa-linkedin"></span></a>
-              </div>
-            </div>
-          </div>
-        </div>
-  
-      </div>
-       <div class="col-6">
-  
-        <div class="box p-3 shadow bg-white rounded my-3">
-          <div class="row">
-            <div class="col-4">
-              <img src="https://res.cloudinary.com/dpnea22ek/image/upload/v1627660134/user3.png" alt="" width="100%">
-            </div>
-            <div class="col-8">
-              <h4>Dianna C. Avila</h4>
-              <p class="my-2"><small>Graphic Designer</small></p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing tristique hendrerit laoreet.</p>
-              <div class="social-icons">
-                <a href="#" class="me-2"><span class="fa fa-facebook"></span></a><a href="#" class="me-2"><span class="fa fa-instagram"></span></a><a href="#" class="me-2"><span class="fa fa-twitter"></span></a><a href="#" class="me-2"><span class="fa fa-linkedin"></span></a>
-              </div>
-            </div>
-          </div>
-        </div>
-  
-      </div>
-       <div class="col-6">
-  
-        <div class="box p-3 shadow bg-white rounded my-3">
-          <div class="row">
-            <div class="col-4">
-              <img src="https://res.cloudinary.com/dpnea22ek/image/upload/v1627660147/user2.png" alt="" width="100%">
-            </div>
-            <div class="col-8">
-              <h4>Robert C. Young</h4>
-              <p class="my-2"><small>Web Designer</small></p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing tristique hendrerit laoreet.</p>
-              <div class="social-icons">
-                <a href="#" class="me-2"><span class="fa fa-facebook"></span></a><a href="#" class="me-2"><span class="fa fa-instagram"></span></a><a href="#" class="me-2"><span class="fa fa-twitter"></span></a><a href="#" class="me-2"><span class="fa fa-linkedin"></span></a>
-              </div>
-            </div>
-          </div>
-        </div>
-  
-      </div>
-  </div>`
+</section>`
   },
   {
     id: 'section-footer',
